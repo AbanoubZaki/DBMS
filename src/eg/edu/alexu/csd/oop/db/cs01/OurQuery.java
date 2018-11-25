608f85aa7c94a4f3f85d2ec3309129b4a907aa76
DELL <DELL@michael>
2018-11-25 21:03:35 +0200

    Database is done

--- src/eg/edu/alexu/csd/oop/db/cs01/OurQuery.java
+++ src/eg/edu/alexu/csd/oop/db/cs01/OurQuery.java
@@ -3,7 +3,6 @@ package eg.edu.alexu.csd.oop.db.cs01;
 public abstract class OurQuery implements IQuery{
 
 	private String table = new String();
-	
 	@Override
 	public void setTable(String table) {
 		this.table = table;
--- src/eg/edu/alexu/csd/oop/db/cs01/modules/DataBase.java
+++ src/eg/edu/alexu/csd/oop/db/cs01/modules/DataBase.java
@@ -1,5 +1,45 @@
 package eg.edu.alexu.csd.oop.db.cs01.modules;
 
+import java.util.ArrayList;
+
 public class DataBase {
 
+	private String name;
+	private ArrayList<Table>tables;
+	public DataBase(String dataBaseName){
+		this.name=dataBaseName;
+		tables = new ArrayList<Table>();
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the tables
+	 */
+	public ArrayList<Table> getTables() {
+		return tables;
+	}
+	/**
+	 * @param tables the tables to set
+	 */
+	public void setTables(ArrayList<Table> tables) {
+		this.tables = tables;
+	}
+	
+	public Table getTable(String nameTable) {
+		for(Table T:tables) {
+			if(T.getTableName().equals(nameTable))
+				return T;
+		}
+		return null;
+	}
 }
