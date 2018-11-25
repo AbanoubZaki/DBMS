142bdef975d26f803869b64af71ac134229d2310
Abanoub Ashraf <Abanoub Ashraf@DESKTOP-17KSTCF>
2018-11-25 20:57:45 +0200

    table & cell done

--- src/eg/edu/alexu/csd/oop/db/cs01/modules/Cell.java
+++ src/eg/edu/alexu/csd/oop/db/cs01/modules/Cell.java
@@ -1,5 +1,23 @@
 package eg.edu.alexu.csd.oop.db.cs01.modules;
 
 public class Cell {
-
+	private String type = new String();
+	
+	private String vlaue = new String();
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setValue(String value) {
+		this.vlaue = value;
+	}
+	
+	public String getValue() {
+		return vlaue;
+	}
 }
--- src/eg/edu/alexu/csd/oop/db/cs01/modules/Table.java
+++ src/eg/edu/alexu/csd/oop/db/cs01/modules/Table.java
@@ -1,5 +1,38 @@
 package eg.edu.alexu.csd.oop.db.cs01.modules;
 
-public class Table {
+import java.util.ArrayList;
 
+public class Table {
+	
+	private String tableName = new String();
+	
+	private ArrayList<String> columnNames;
+	
+	private ArrayList<Row> table = new ArrayList<>();
+	
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	
+	public String getTableName() {
+		return tableName;
+	}
+	
+	public void setColumnNames(ArrayList<String> columnNames) {
+		this.columnNames = new ArrayList<>(columnNames);
+	}
+	
+	public ArrayList<String> getColumnNames() {
+		return columnNames;
+	}
+	
+	public void addRow(Row row) {
+		table.add(row);
+	}
+	
+	public Row getRow(int index) {
+		return table.get(index);
+	}
+	
+	
 }
