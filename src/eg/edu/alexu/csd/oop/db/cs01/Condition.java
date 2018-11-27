2984d4e0f369e50efed12a3bc25038db47d9ecc4
Kirellos Malak Habib <42212651+KiroMalakHabib@users.noreply.github.com>
2018-11-27 04:12:21 +0200

    filling Condition class parse : SELECT * FROM table_name where condition

--- src/eg/edu/alexu/csd/oop/db/cs01/Condition.java
+++ src/eg/edu/alexu/csd/oop/db/cs01/Condition.java
@@ -1,5 +1,14 @@
 package eg.edu.alexu.csd.oop.db.cs01;
 
 public class Condition {
-
+	private String condition = new String();
+	public Condition(String condition) {
+		this.setCondition(condition);
+	}
+	public String getCondition() {
+		return condition;
+	}
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
 }
--- src/eg/edu/alexu/csd/oop/db/cs01/Parser.java
+++ src/eg/edu/alexu/csd/oop/db/cs01/Parser.java
@@ -12,6 +12,7 @@ import eg.edu.alexu.csd.oop.dp.cs01.queries.DropDatabase;
 import eg.edu.alexu.csd.oop.dp.cs01.queries.DropTable;
 import eg.edu.alexu.csd.oop.dp.cs01.queries.IQuery;
 import eg.edu.alexu.csd.oop.dp.cs01.queries.InsertInto;
+import eg.edu.alexu.csd.oop.dp.cs01.queries.SelectFrom;
 
 public class Parser {
 
@@ -36,8 +37,9 @@ public class Parser {
 		final String drobDataBasePattern = "drob database (\\w+) ?; ?";
 		final String createTablePattern = "create table (\\w+) \\( ?(( ?\\w+ (int|varchar) ?,?)+)\\) ?; ?";
 		final String drobTablePattern = "drob table (\\w+) ?; ?";
-		final String insertIntoTableColumnsAndValues = "insert into (\\w+) (\\( ?(( ?\\w+ ?,?)+)\\)) values \\((( ?\\w+ ?,?)+)\\) ?; ?";
-		final String insertIntoTableValuesOnly = "insert into (\\w+) values \\( ?(( ?\\w+ ?,?)+)\\) ?; ?";
+		final String insertIntoTableColumnsAndValuesPattern = "insert into (\\w+) (\\( ?(( ?\\w+ ?,?)+)\\)) values \\((( ?\\w+ ?,?)+)\\) ?; ?";
+		final String insertIntoTableValuesOnlyPattern = "insert into (\\w+) values \\( ?(( ?\\w+ ?,?)+)\\) ?; ?";
+		final String selectAllFromTablePattern = "select \\* from (\\w+) ?((where) ?((not)? ?(\\S+) ? [!=><]+ ?(\\S+) ?(or|and)? ?(not)? ?(\\S+) ? [!=><]+ ?(\\S+) ?))? ?; ?";
 
 		// Adding regex-es to the ArrayList.
 		ArrayList<String> allPatternStrings = new ArrayList<>();
@@ -45,8 +47,9 @@ public class Parser {
 		allPatternStrings.add(drobDataBasePattern);// 1
 		allPatternStrings.add(createTablePattern);// 2
 		allPatternStrings.add(drobTablePattern);// 3
-		allPatternStrings.add(insertIntoTableColumnsAndValues);// 4
-		allPatternStrings.add(insertIntoTableValuesOnly);// 5
+		allPatternStrings.add(insertIntoTableColumnsAndValuesPattern);// 4
+		allPatternStrings.add(insertIntoTableValuesOnlyPattern);// 5
+		allPatternStrings.add(selectAllFromTablePattern);//6
 
 		// ArrayLists for patterns and matchers.
 		ArrayList<Pattern> thePatterns = new ArrayList<>();
@@ -68,13 +71,15 @@ public class Parser {
 		 */
 		theQuery = theQuery.replaceAll("( +)", " ");
 		theQuery = theQuery.toLowerCase();
-		
-		//Creating the right query.
+
+		// Creating the right query.
 		if (theQuery.contains(";") && theMatchers.get(0).find()) {// if the query match create data base.
 			IQuery createDataBaseQuery = new CreateDatabase(theMatchers.get(0).group(1), false);
+			//group(1) is the name of the data base.
 			theMainDataBase = theMatchers.get(0).group(1);
 			return createDataBaseQuery;
 		} else if (theQuery.contains(";") && theMatchers.get(1).find()) {// if the query match drop data base.
+			//group(1) is the name of the data base.
 			IQuery drobDataBaseQuery = new DropDatabase(theMatchers.get(1).group(1));
 			return drobDataBaseQuery;
 		} else if (theQuery.contains(";") && theMatchers.get(2).find()) {// if the query match create table.
@@ -83,6 +88,7 @@ public class Parser {
 				return null;
 			}
 			String[] coulmns;
+			//group(2) is a string contains all columns beside their types example (column_1 type, ...).
 			coulmns = theMatchers.get(2).group(2).split(" ?, ?");
 			ArrayList<String> coulmnsName = new ArrayList<>();
 			ArrayList<String> coulmnsType = new ArrayList<>();
@@ -90,10 +96,13 @@ public class Parser {
 			for (int i = 0; i < coulmns.length; i++) {
 				Matcher coulmnsNameAndTypeMatcher = p.matcher(coulmns[i]);
 				if (coulmnsNameAndTypeMatcher.find()) {
+					//group(1) is column names.
 					coulmnsName.add((String) coulmnsNameAndTypeMatcher.group(1));
+					//group(2) is column types.
 					coulmnsType.add((String) coulmnsNameAndTypeMatcher.group(2));
 				}
 			}
+			//group(1) is the name of the table.
 			Table createTable = new Table(theMainDataBase, theMatchers.get(2).group(1), coulmnsName, coulmnsType);
 			IQuery createTableQuery = new CreateTable(createTable);
 			return createTableQuery;
@@ -102,38 +111,61 @@ public class Parser {
 				System.out.println("There is NO DataBase selected");
 				return null;
 			}
+			//group(1) is the name of the table.
 			Table dropTable = new Table(theMainDataBase, theMatchers.get(3).group(1));
 			IQuery dropTableQuery = new DropTable(dropTable);
 			return dropTableQuery;
-		} else if (theQuery.contains(";") && theMatchers.get(4).find()) {// if the query match insert Into Table Columns And Values.
+		} else if (theQuery.contains(";") && theMatchers.get(4).find()) {// if the query match insert Into Table Columns
+																			// And Values.
 			if (theMainDataBase == null) {
 				System.out.println("There is NO DataBase selected");
 				return null;
 			}
 			String[] coulmnsArray;
 			String[] valuesArray;
+			//group(3) is column names string.
 			coulmnsArray = theMatchers.get(4).group(3).split(" ?, ?");
+			//group(5) is values of each column string.
 			valuesArray = theMatchers.get(4).group(5).split(" ?, ?");
 			if (coulmnsArray.length != valuesArray.length) {
 				System.out.println("could not prepare statement.");
 				return null;
 			}
+			// converting array to ArrayList.
 			ArrayList<String> columnNames = new ArrayList<String>(Arrays.asList(coulmnsArray));
+			// converting array to ArrayList.
 			ArrayList<String> values = new ArrayList<String>(Arrays.asList(valuesArray));
+			//group(1) is the name of the table.
 			Table insertIntoTableColumnsAndValuesTable = new Table(theMainDataBase, theMatchers.get(4).group(1));
-			IQuery insertIntoTableColumnsAndValuesQuery = new InsertInto(insertIntoTableColumnsAndValuesTable, columnNames, values);
+			IQuery insertIntoTableColumnsAndValuesQuery = new InsertInto(insertIntoTableColumnsAndValuesTable,
+					columnNames, values);
 			return insertIntoTableColumnsAndValuesQuery;
-		} else if (theQuery.contains(";") && theMatchers.get(5).find()) {// if the query match insert Into Table Values Only.
+		} else if (theQuery.contains(";") && theMatchers.get(5).find()) {// if the query match insert Into Table Values
+																			// Only.
 			if (theMainDataBase == null) {
 				System.out.println("There is NO DataBase selected");
 				return null;
 			}
 			String[] valuesArray;
+			//group(2) is a string contains all values for the columns.
 			valuesArray = theMatchers.get(5).group(2).split(" ?, ?");
+			// converting array to ArrayList.
 			ArrayList<String> values = new ArrayList<String>(Arrays.asList(valuesArray));
+			//group(1) is the name of the table.
 			Table insertIntoTableValuesOnlyTable = new Table(theMainDataBase, theMatchers.get(5).group(1));
 			IQuery insertIntoTableValuesOnlyQuery = new InsertInto(insertIntoTableValuesOnlyTable, values);
 			return insertIntoTableValuesOnlyQuery;
+		} else if (theQuery.contains(";") && theMatchers.get(6).find()) {// select * from tabel_name where condition.
+			if (theMainDataBase == null) {
+				System.out.println("There is NO DataBase selected");
+				return null;
+			}
+			//group(1) is the name of the table.
+			Table tableSelectAllFromTable = new Table(theMainDataBase, theMatchers.get(6).group(1));
+			//group(4) is the condition it may equals null.
+			Condition selectAllFromTableCondition = new Condition(theMatchers.get(6).group(4));
+			IQuery selectAllFromTableQuery = new SelectFrom(tableSelectAllFromTable, selectAllFromTableCondition);
+			return selectAllFromTableQuery;
 		}
 		return null;
 	}
