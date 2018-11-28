4fc9efd6abbcc026351c8e232b13048992f0428d
Arsany <Arsany@DESKTOP-07O58V4>
2018-11-27 19:58:52 -0800

    reading table in delete class

--- src/eg/edu/alexu/csd/oop/dp/cs01/queries/DeleteFrom.java
+++ src/eg/edu/alexu/csd/oop/dp/cs01/queries/DeleteFrom.java
@@ -1,6 +1,7 @@
 package eg.edu.alexu.csd.oop.dp.cs01.queries;
 
 import eg.edu.alexu.csd.oop.db.cs01.Condition;
+import eg.edu.alexu.csd.oop.db.cs01.fileManager.FileManager;
 import eg.edu.alexu.csd.oop.db.cs01.modules.Table;
 
 public class DeleteFrom extends OurQuery {
@@ -12,8 +13,7 @@ public class DeleteFrom extends OurQuery {
 	
 	@Override
 	public boolean execute() {
-
-		
+		FileManager.getInstance().readTable(getTable());
 		if (getCondition() == null) {
 			getTable().setRows(null);
 		}else if (getCondition() != null) {
