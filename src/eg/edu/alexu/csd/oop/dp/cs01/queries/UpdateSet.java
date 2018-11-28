861f5be061fc9037ec5b2c72302a90ea31b55195
Arsany <Arsany@DESKTOP-07O58V4>
2018-11-27 18:13:03 -0800

    updateset

--- src/eg/edu/alexu/csd/oop/dp/cs01/queries/UpdateSet.java
+++ src/eg/edu/alexu/csd/oop/dp/cs01/queries/UpdateSet.java
@@ -1,46 +1,54 @@
 package eg.edu.alexu.csd.oop.dp.cs01.queries;
 
+import java.util.ArrayList;
+
 import eg.edu.alexu.csd.oop.db.cs01.Condition;
+import eg.edu.alexu.csd.oop.db.cs01.fileManager.FileManager;
 import eg.edu.alexu.csd.oop.db.cs01.modules.Cell;
 import eg.edu.alexu.csd.oop.db.cs01.modules.Table;
 
 public class UpdateSet extends OurQuery {
 
-	private String columnName;
+	private ArrayList<String> columnNames;
 
-	private String value;
+	private ArrayList<String> values;
 	
 	
-	public UpdateSet(Table table, String columnName, String value, Condition condition) {
-		this.columnName = columnName;
-		this.value = value;
+	public UpdateSet(Table table, ArrayList<String> columnNames, ArrayList<String> values, Condition condition) {
+		this.columnNames = columnNames;
+		this.values = values;
 		setTable(table);
 		setCondition(condition);
 	}
 	
 	@Override
 	public boolean execute() {
-		if(!getTable().getColumnNames().contains(columnName)) {
-			return false;
+		FileManager.getInstance().readTable(getTable());
+		for(int i = 0; i < getTable().getColumnNames().size(); i++) {
+			if(!getTable().getColumnNames().contains(columnNames.get(i))) {
+				return false;
+			}	
 		}
-		int index = 0;
-		if(getCondition() == null) {
-			for (int j = 0; j < getTable().getColumnNames().size(); j++) {
-				if(columnName.equals(getTable().getColumnNames().get(j))) {
-					index = j;
-					break;
+		
+		int index = -1;
+		for (int k = 0; k < columnNames.size(); k++) {
+			if(getCondition() == null) {
+				for (int j = 0; j < getTable().getColumnNames().size(); j++) {
+					if(columnNames.get(index).equals(getTable().getColumnNames().get(j))) {
+						index = j;
+						break;
+					}
 				}
+				for (int i = 0; i < getTable().getRows().size(); i++) {
+						getTable().getRows().get(index).updateCell(columnNames.get(k), new Cell(values.get(k)));
+				}
+			}else {
+				
+				
+				
 			}
-			for (int i = 0; i < getTable().getRows().size(); i++) {
-					getTable().getRows().get(index).updateCell(columnName, new Cell(value));
-			}
-		}else {
-			
-			
-			
+		
 		}
-		
-		
 		
 		
 		return true;
